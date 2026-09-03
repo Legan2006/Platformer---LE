c5f14c5664570d44d34d99fc79593b2823b6b674
Legan2006 <mrmanl.egan@yahoo.com>
2026-09-02 17:17:57 -0700

    Basic movement

--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -4,26 +4,45 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private InputActionAsset inputActions;
-    InputAction moveAction;
 
+
+    [SerializeField] private float runspeed = 5;
+
+    [SerializeField] private InputActionAsset inputActions;
+
+    InputAction moveAction;
+    public Vector2 MoveInput { get; private set; }
+
+    Rigidbody2D player;
     void Awake()
     {
+        player = GetComponent<Rigidbody2D>();
+
         InputActionMap playerMap = inputActions.FindActionMap("PlayerCharacter" , true);
 
         moveAction = playerMap.FindAction("Move" , true);
 
         playerMap.Enable();
+
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        MoveInput = moveAction.ReadValue<Vector2>();
+        Run();
     }
+
+    private void Run()
+    {
+        float hMovement = MoveInput.x;
+        player.linearVelocity = new Vector2(hMovement * runspeed, player.linearVelocity.y);
+    }
+
 }
