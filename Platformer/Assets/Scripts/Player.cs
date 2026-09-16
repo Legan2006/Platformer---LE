08b86c1b5273966a80bff6120e32622a922dce98
Legan2006 <mrmanl.egan@yahoo.com>
2026-09-16 15:47:56 -0700

    player jumping

--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -1,48 +1,116 @@
-using System.Transactions;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float runSpeed = 5.0f;
 
+    [SerializeField] private float runAcceleration = 30f;
 
-    [SerializeField] private float runspeed = 5;
-
+    [SerializeField] private float runDeceleration = 40f;
+    
     [SerializeField] private InputActionAsset inputActions;
 
-    InputAction moveAction;
-    public Vector2 MoveInput { get; private set; }
+    [SerializeField] private float jumpSpeed = 5f;
 
-    Rigidbody2D player;
+    [SerializeField] private LayerMask groundLayer;
+    InputAction moveAction;
+
+    InputAction jumpAction;
+
+    public bool jumpPressedThisFrame => jumpAction != null && jumpAction.WasPressedThisFrame();
+    public LayerMask GroundLayer => groundLayer.value != 0 ? groundLayer : LayerMask.GetMask("Ground");
+    public Vector2 MoveInput { get; private set; }
+    
+    Rigidbody2D playerCharacter;
+    
+    Animator playerAnimator;
+
+    BoxCollider2D playerFeetCollider;
+
+    // Initializes its contents before the game begins
     void Awake()
     {
-        player = GetComponent<Rigidbody2D>();
+        playerCharacter = GetComponent<Rigidbody2D>();
 
-        InputActionMap playerMap = inputActions.FindActionMap("PlayerCharacter" , true);
+        playerAnimator = GetComponentInChildren<Animator>();
+        
+        InputActionMap playerMap = inputActions.FindActionMap("Player", true);
 
-        moveAction = playerMap.FindAction("Move" , true);
+        moveAction = playerMap.FindAction("Move", true);
+
+        jumpAction = playerMap.FindAction("Jump", true);
+
+        playerFeetCollider = GetComponent<BoxCollider2D>();
 
         playerMap.Enable();
 
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
         MoveInput = moveAction.ReadValue<Vector2>();
+
         Run();
+        FlipSprite();
+        Jump();
     }
 
     private void Run()
     {
         float hMovement = MoveInput.x;
-        player.linearVelocity = new Vector2(hMovement * runspeed, player.linearVelocity.y);
+
+        float targetSpeed = MoveInput.x * runSpeed;
+
+        float speedChange;
+
+        if(Mathf.Abs(targetSpeed) > Mathf.Epsilon)
+        {
+            speedChange = runAcceleration;
+        }
+        else
+        {
+            speedChange = runDeceleration;
+        }
+
+        float newSpeed = Mathf.MoveTowards(playerCharacter.linearVelocity.x, targetSpeed, speedChange * Time.deltaTime);
+
+        playerCharacter.linearVelocity = new Vector2(newSpeed, playerCharacter.linearVelocity.y);
+
+        bool hSpeed = Mathf.Abs(playerCharacter.linearVelocity.x) > Mathf.Epsilon;
+
+        playerAnimator.SetBool("run", hSpeed);
     }
+
+    private void FlipSprite()
+    {
+        Boolean hMovement = Mathf.Abs(playerCharacter.linearVelocity.x) > Mathf.Epsilon;
+
+        if (hMovement)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerCharacter.linearVelocity.x), 1f);
+        }
+    }
+    
+    private void Jump()
+    {
+        bool isGrounded = playerFeetCollider.IsTouchingLayers(GroundLayer);
+
+        if (jumpPressedThisFrame && isGrounded)
+        {
+            playerCharacter.linearVelocity = new Vector2(playerCharacter.linearVelocityX, jumpSpeed) ;
+        }
+    }
+
+
 
 }
